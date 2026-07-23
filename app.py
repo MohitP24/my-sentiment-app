65fe6484b9bc953bc42ff2da084ca0c57db70781
MohitP24 <mohit10patil24@gmail.com>
2026-07-24 02:47:39 +0530

    Configure app for Render deployment

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 import gradio as gr
 from transformers import pipeline
 
@@ -26,4 +27,7 @@ app = gr.Interface(
     description="Type any sentence and I will tell you whether it is Positive or Negative!",
 )
 
-app.launch()
+app.launch(
+    server_name="0.0.0.0",
+    server_port=int(os.environ.get("PORT", 7860))
+)
